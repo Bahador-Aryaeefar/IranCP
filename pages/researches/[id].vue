1b2d10157de752af45949daf0b3507b71bab5243
Bahador-Aryaeefar <bahadoraryaeefar@gmail.com>
2023-11-29 12:43:44 +0330

    more admin

--- pages/researches/[id].vue
+++ pages/researches/[id].vue
@@ -2,6 +2,7 @@
     <div class="pb-20">
         <ResearchExpert v-if="user.role_id == 4" class="mt-10"></ResearchExpert>
         <ResearchReferee v-if="user.role_id == 3" class="mt-10"></ResearchReferee>
+        <ResearchAdmin v-if="user.role_id == 1 || user.role_id == 2" class="mt-10"></ResearchAdmin>
     </div>
 </template>
 
